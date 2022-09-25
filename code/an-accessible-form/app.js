59addc4d6e3cf0063d57bae4a396e985d46571c8
aaron <alkrauss48@gmail.com>
2022-09-25 17:32:09 -0500

    Display error and success messages

--- code/an-accessible-form/app.js
+++ code/an-accessible-form/app.js
@@ -9,15 +9,58 @@ const onFormSubmit = (event) => {
   const formData = new FormData(event.target);
   const formProps = Object.fromEntries(formData);
 
-  // Get the right 'thank you' message, based on the user's 'enjoying' response.
+  validate(formProps);
+};
+
+const validate = (formProps) => {
+  // Get all form fields
+  const nameError = document.getElementById('nameError');
+  const emailError = document.getElementById('emailError');
+  const enjoyingError = document.getElementById('enjoyingError');
+  const success = document.getElementById('success');
+
+  // The object representing the errors
+  // Key - formProps key name, Value - the client-facing error message.
+  let errors = {};
+
+  // Hide all the error fields, so we can conditionally show them if they have an error.
+  nameError.classList.add('is-hidden');
+  emailError.classList.add('is-hidden');
+  enjoyingError.classList.add('is-hidden');
+  success.classList.add('is-hidden');
+
+  // Handle requiring the Name field.
+  if (!formProps.name) {
+    errors.name = 'Name is required.';
+    nameError.innerHTML = errors.name;
+    nameError.classList.remove('is-hidden');
+  }
+
+  // Handle requiring the Email field.
+  if (!formProps.email) {
+    errors.email = 'Email is required.';
+    emailError.innerHTML = errors.email;
+    emailError.classList.remove('is-hidden');
+  }
+
+  // Handle requiring the 'Are you Enjoying TP2022' field.
+  if (!formProps.enjoying) {
+    errors.enjoying = 'Knowing that you are enjoying ThunderPlains 2022 is required.';
+    enjoyingError.innerHTML = errors.enjoying;
+    enjoyingError.classList.remove('is-hidden');
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return;
+  }
+
   const thankYou = getThankYouMessage(formProps.enjoying);
 
-  // Print the results
-  const results = document.getElementById('results');
-
-  results.innerHTML =
+  success.innerHTML =
     `${thankYou}, ${formProps.name} (${formProps.email})`;
-};
+
+  success.classList.remove('is-hidden');
+}
 
 const getThankYouMessage = (enjoying) => {
   switch(enjoying) {
