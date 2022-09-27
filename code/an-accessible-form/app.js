730e4aa0fa94e8caebf193d1c88f7a159023899c
aaron <alkrauss48@gmail.com>
2022-09-26 22:44:35 -0500

    Implement aria roles and attributes to form

--- code/an-accessible-form/app.js
+++ code/an-accessible-form/app.js
@@ -2,64 +2,72 @@ const YES_ENGLISH = 'yes-english';
 const YES_GERMAN = 'yes-german';
 const YES_SPANISH = 'yes-spanish';
 
+
 const onFormSubmit = (event) => {
   event.preventDefault();
 
   // Process the form data into an object.
   const formData = new FormData(event.target);
-  const formProps = Object.fromEntries(formData);
+  const data = Object.fromEntries(formData);
 
-  validate(formProps);
+  const errors = validate(data);
+
+  if (Object.keys(errors).length > 0) {
+    handleErrors(errors);
+    return;
+  }
+
+  handleSuccess(data);
+};
+
+const handleSuccess = (data) => {
+  const thankYou = getThankYouMessage(data.enjoying);
+
+  const success = document.getElementById('form')
+    .appendChild(document.createElement('p'))
+
+  success.innerText =
+    `${thankYou}, ${data.name} (${data.email})`;
+
+  success.classList.add('success', 'validation');
+};
+
+const handleErrors = (errors) => {
+  for (const name in errors) {
+    const message = errors[name];
+
+    handleValidationError(name, message);
+  }
 };
 
 const validate = (formProps) => {
-  // Get all form fields
-  const nameError = document.getElementById('nameError');
-  const emailError = document.getElementById('emailError');
-  const enjoyingError = document.getElementById('enjoyingError');
-  const success = document.getElementById('success');
-
   // The object representing the errors
   // Key - formProps key name, Value - the client-facing error message.
   let errors = {};
 
-  // Hide all the error fields, so we can conditionally show them if they have an error.
-  nameError.classList.add('is-hidden');
-  emailError.classList.add('is-hidden');
-  enjoyingError.classList.add('is-hidden');
-  success.classList.add('is-hidden');
+  resetValidation();
 
   // Handle requiring the Name field.
   if (!formProps.name) {
     errors.name = 'Name is required.';
-    nameError.innerHTML = errors.name;
-    nameError.classList.remove('is-hidden');
   }
 
   // Handle requiring the Email field.
   if (!formProps.email) {
     errors.email = 'Email is required.';
-    emailError.innerHTML = errors.email;
-    emailError.classList.remove('is-hidden');
+  }
+
+  // Handle formatting the Email field.
+  if (!errors.email && !formProps.email.match(/^\S+@\S+\.\S+$/)) {
+    errors.email = 'Must provide a valid email address.';
   }
 
   // Handle requiring the 'Are you Enjoying TP2022' field.
   if (!formProps.enjoying) {
     errors.enjoying = 'Knowing that you are enjoying ThunderPlains 2022 is required.';
-    enjoyingError.innerHTML = errors.enjoying;
-    enjoyingError.classList.remove('is-hidden');
   }
 
-  if (Object.keys(errors).length > 0) {
-    return;
-  }
-
-  const thankYou = getThankYouMessage(formProps.enjoying);
-
-  success.innerHTML =
-    `${thankYou}, ${formProps.name} (${formProps.email})`;
-
-  success.classList.remove('is-hidden');
+  return errors;
 }
 
 const getThankYouMessage = (enjoying) => {
@@ -71,4 +79,31 @@ const getThankYouMessage = (enjoying) => {
   }
 
   return 'Thank You';
+};
+
+const resetValidation = () => {
+  document.querySelectorAll('.validation').forEach( elem => elem.remove() );
+
+  document.querySelectorAll('[aria-invalid]').forEach( (elem) => {
+    elem.removeAttribute('aria-invalid');
+  });
+
+  document.querySelectorAll('[aria-errormessage]').forEach( (elem) => {
+    elem.removeAttribute('aria-errormessage');
+  });
+}
+
+const handleValidationError = (name, message) => {
+  const formControl = document.querySelector(`[name=${name}]`);
+  const formGroup = formControl.closest('.form-group');
+
+  const errorElement = formGroup.appendChild(document.createElement('p'))
+
+  errorElement.id = `${name}Error`;
+  errorElement.innerText = message;
+  errorElement.setAttribute('role', 'alert');
+  errorElement.classList.add('error', 'validation');
+
+  formControl.setAttribute('aria-invalid', 'true');
+  formControl.setAttribute('aria-errormessage', errorElement.id);
 };
